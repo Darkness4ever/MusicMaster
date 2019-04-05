e8fe4e909da2816174f8500916f850cde8f465bb
mahapata <ankit.mahapatra@cdk.com>
2019-04-05 21:44:51 +0530

    temp ...

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Artist from './Artist'
+import Tracks from './Tracks'
+import Search from './Search'
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com'
+
+class App extends Component {
+    state = {
+        artist : null,
+        tracks : []
+    }
+
+
+    searchArtist = artistQuery => {
+        fetch(`${API_ADDRESS}/artist/${this.artistQuery}`)
+        .then(res => res.json())
+        .then(json => {
+            if(json.artists.total > 0) {
+                const artist = json.artists.items[0]
+                this.setState({artist})
+
+                fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(res => res.json())
+                .then(json => this.setState({tracks : json.tracks}))
+                .catch(err => alert(err.message))
+            }
+        })
+        .catch(err => alert(err.message))
+    }
+
+    
+
+    render() {
+        console.log('state  : ', this.state)
+        
+        return (
+            <div >
+                <Search  searchArtist = {this.searchArtist} />
+                <h2>Music Master</h2>
+                 <Artist artist = {this.state.artist}/>
+                <Tracks tracks = {this.state.tracks} />
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
